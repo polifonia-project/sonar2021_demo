be8fb907dca54bfca9b01065853c65f8a10f8e8e
colin <colin@ubuntu.com>
2021-09-15 09:57:23 -0400

    cleaned develop branch

--- src/main.ts
+++
@@ -1,9 +0,0 @@
-import {AudioPlayer} from "./audio/AudioPlayer"
-
-declare global {
-    interface Window {
-        AudioPlayer: any
-    }
-}
-
-window.AudioPlayer = AudioPlayer
--- test/testSetup.js
+++
@@ -1,4 +0,0 @@
-require('dotenv').config()
-
-// in CI some tasks goes in timeout
-jest.setTimeout(100000);
