92abb6c1849ca55b3faeb1652d5c286a22a145aa
ccolonna <mail@example.com>
2021-09-30 10:47:01 +0200

    when annotation appears it gradually acquire full opacity and a border with annotation color shine for 1s

--- src/app/stream-item/stream-item.component.ts
+++ src/app/stream-item/stream-item.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import {Annotation} from '../annotation';
+import { AnnotationGraphics } from '../annotation-graphics';
+import { AnnotationGraphicsService } from '../annotation-graphics.service';
 
 @Component({
   selector: 'app-stream-item',
@@ -8,11 +10,23 @@ import {Annotation} from '../annotation';
 })
 export class StreamItemComponent implements OnInit {
   @Input() annotation?: Annotation;
+  isRendered: boolean;
+  annotationGraphics?: AnnotationGraphics
 
-  constructor(
-  ) { }
 
+  constructor(private annotationGraphicsService: AnnotationGraphicsService) {
+  }
+  
   ngOnInit(): void {
-
+    if (this.annotation) {
+      this.annotationGraphics = this.annotationGraphicsService.getAnnotationGraphics(this.annotation.type)
+    }
+  }
+  
+  ngAfterContentInit() {
+    setTimeout(
+      () => {
+        this.isRendered = true
+      }, 10);    
   }
 }
